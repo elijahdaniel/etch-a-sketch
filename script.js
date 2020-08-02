19a094c05dd33254b38b2dee23bc1c78ad45f526
elijahdaniel <e.paminsan@gmail.com>
2020-08-02 13:47:07 -0700

    update styling

--- script.js
+++ script.js
@@ -1,29 +1,30 @@
-// grid 
+// grid
 function createGrid(num) {
-for (var i = 0; i < num; i++) {
+  for (var i = 0; i < num; i++) {
     for (var j = 0; j < num; j++) {
-        var divGrid = document.createElement('div');
-        divGrid.style.width = '16px';
-        divGrid.style.height = '16px';
-        divGrid.style.background = 'white';
-        divGrid.style.border = '1px solid white';
-        divGrid.addEventListener('mouseover', hoverOver);
-        container.appendChild(divGrid);
+      var divGrid = document.createElement('div')
+      divGrid.style.width = '16px'
+      divGrid.style.height = '16px'
+      divGrid.style.background = 'white'
+      divGrid.style.border = '1px solid aliceblue'
+      divGrid.addEventListener('mouseover', hoverOver)
+      container.appendChild(divGrid)
     }
-    var breakGrid = document.createElement('br');
-    container.appendChild(breakGrid);
-    }
+    var breakGrid = document.createElement('br')
+    container.appendChild(breakGrid)
+  }
 }
 
 // eventListener
 function hoverOver() {
-    event.target.style.backgroundColor = "rgb(" + randomColor() + ","  + randomColor() + "," + randomColor() + ")";
+  event.target.style.backgroundColor =
+    'rgb(' + randomColor() + ',' + randomColor() + ',' + randomColor() + ')'
 }
 
 // random color picker
 function randomColor() {
-    let randomColor = Math.floor(Math.random() * 255);
-    return randomColor;
+  let randomColor = Math.floor(Math.random() * 255)
+  return randomColor
 }
 
-createGrid(35);
+createGrid(35)
